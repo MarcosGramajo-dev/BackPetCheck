ca0996c192117f47f437dd69a2e4aaddeb439542
Marcos Gramajo <marcosgramajo96@gmail.com>
2023-08-07 22:05:18 -0300

    Sselect search

--- src/controllers/historyControllers.ts
+++ src/controllers/historyControllers.ts
@@ -35,8 +35,43 @@ export async function newHistory(req: Request, res: Response){
 }
 
 export async function findHC (req: Request, res: Response){
+    console.log('===> req', req)
     try {
         const HC = ModelHistory.findOne({id:req.params.id}).then( doc => {
+            if(doc){
+                console.log("====> HC", doc )
+                res.send(doc)
+            } else {
+                res.send("No existe ese ID")
+            }
+        })
+        console.log("=========> Esta en findHC", req.params.id)
+    } catch (error) {
+        console.log("=====> Error", error)
+    }
+}
+
+export async function findHCByDni (req: Request, res: Response){
+    try {
+        const HC = ModelHistory.findOne({'ownerPet.DNI': req.params.id}).then( doc => {
+            if(doc){
+                console.log("=========> Esta en findHC", req.params.id)
+                console.log("====> HC", doc )
+                res.send(doc)
+            } else {
+                res.send(req.params.id)
+            }
+        })
+        console.log("=========> Esta en findHC", req.params.id)
+    } catch (error) {
+        console.log("=====> Error", error)
+        res.send(error)
+    }
+}
+
+export async function findHCByNchip (req: Request, res: Response){
+    try {
+        const HC = ModelHistory.findOne({'DataPet.Nchip': req.params.id}).then( doc => {
             if(doc){
                 console.log("====> HC", doc )
                 res.send(doc)
--- src/routes/vetsRoutes.ts
+++ src/routes/vetsRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { GetAll } from '../controllers/vetsControllers';
-import { findHC } from '../controllers/historyControllers';
+import { findHC, findHCByDni, findHCByNchip } from '../controllers/historyControllers';
 const router = Router();
 
 router.get('/', GetAll);
 
-router.get('/HistoryClinic/:id', findHC);
+router.get('/HistoryClinic/id/:id', findHC);
+router.get('/HistoryClinic/dni/:id', findHCByDni);
+router.get('/HistoryClinic/nchip/:id', findHCByNchip);
 
 export default router;
