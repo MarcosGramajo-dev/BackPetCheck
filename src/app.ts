af4cb3eb5439ee2f4a8e39d19b57f803d1c7b1ea
Marcos Gramajo <marcosgramajo96@gmail.com>
2023-08-25 20:28:22 -0300

    change cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'http://www.petcheck.com.ar',
 };
 
 //Middlewares
